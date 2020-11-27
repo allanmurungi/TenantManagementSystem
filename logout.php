09445a989e83a0d18bdf75f220fd3d216d636ff8
allan murungi <mcallans25@gmail.com>
2020-11-27 15:07:00 -0800

    updated code 2

--- logout.php
+++ logout.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
 
+
+include("../connection.php");
+include("../constants.php");
+include("../utilityfunctions.php");
+
+include("../connection2.php");
+
+
 if(!isset($_SESSION["un"]) ){
 
 header("location:".DOMAIN_LOGIN);
